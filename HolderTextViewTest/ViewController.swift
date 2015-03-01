56993ca69f68bd8261f110c6f1add75cfcc93cc3
kunpengchen <674027158@qq.com>
2015-03-01 11:15:42 +0800

    Initial Commit

---
+++ HolderTextViewTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolderTextViewTest
+//
+//  Created by 陈鲲鹏 on 15/3/1.
+//  Copyright (c) 2015年 陈鲲鹏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
